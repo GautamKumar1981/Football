f00b583e15e7b876f77a17b75d9943464cce143a
GautamKumar1981 <GAUTAM.KUMAR@LIVE.COM>
2026-03-21 23:44:17 +0000

    Create dashboard page displaying bets and bankroll

---
+++ frontend/app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <h2>Bets</h2>
+      <div>
+        {/* Display bets here */}
+      </div>
+      <h2>Bankroll</h2>
+      <div>
+        {/* Display bankroll here */}
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
