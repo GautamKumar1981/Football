1905fd165b1bb6b2a547d66ab11583544671b7bd
GautamKumar1981 <GAUTAM.KUMAR@LIVE.COM>
2026-03-22 20:43:57 +0000

    Add OrderPanel component

---
+++ frontend/components/OrderPanel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OrderPanel: React.FC = () => {
+    return (
+        <div>
+            <h1>Order Panel</h1>
+            {/* Additional order panel features can be added here */}
+        </div>
+    );
+};
+
+export default OrderPanel;
